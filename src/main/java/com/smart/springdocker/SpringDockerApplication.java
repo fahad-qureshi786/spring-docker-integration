37183df8b7e6e5cf819df59f5ff8469e267cfbc6
fahad-qureshi786 <mfahad.bses19@iba-suk.edu.pk>
2022-03-30 06:01:24 +0500

    create configurations for docker * hit some commands to create docker file in docker server * add readme file

--- src/main/java/com/smart/springdocker/SpringDockerApplication.java
+++ src/main/java/com/smart/springdocker/SpringDockerApplication.java
@@ -2,9 +2,17 @@ package com.smart.springdocker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringDockerApplication {
+
+    @GetMapping("/")
+    public String welcome(){
+        return "Greeting! Tech Soft";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringDockerApplication.class, args);
